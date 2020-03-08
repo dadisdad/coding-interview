83dcd21a0d18c3dda43e76a08094fd11e3e3f3b3
李明 <1030839531@qq.com>
2020-03-08 14:11:23 +0800

    二叉树的下一个节点

---
+++ src/main/java/com/dadisdad/coding/pub/BinaryTreeNode.java
@@ -0,0 +1,17 @@
+package com.dadisdad.coding.pub;
+
+/**
+ * @author 10308
+ * @date 2020/3/8
+ */
+public class BinaryTreeNode {
+
+    public String val;
+    public BinaryTreeNode parent;
+    public BinaryTreeNode left;
+    public BinaryTreeNode right;
+
+    public BinaryTreeNode(String val) {
+        this.val = val;
+    }
+}
